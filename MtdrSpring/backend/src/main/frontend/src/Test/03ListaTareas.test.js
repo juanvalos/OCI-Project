67848493c409711981ce1f0a9dbc19dc4e448459
rox-dotcom <roxarandagr@gmail.com>
2025-04-24 08:54:48 -0600

    Some change of bat

--- MtdrSpring/backend/src/main/frontend/src/Test/03ListaTareas.test.js
+++ MtdrSpring/backend/src/main/frontend/src/Test/03ListaTareas.test.js
@@ -1,5 +1,6 @@
-//import * as React from 'react';
-
+/* Lista de tareas completadas por Sprint. 
+Probar que la información mínima esté presente en el ticket: 
+Nombre Tarea, Nombre de desarrollador, horas estimadas, horas reales. */
 
 test('dummy test', () => {
   expect(true).toBe(true);
