d237f639001a8a1d75af8c44f567d990b32bd5ac
Juan Avalos <111295746+juanvaloss@users.noreply.github.com>
2025-05-29 13:48:14 -0600

    Prueba deploy 4

--- MtdrSpring/backend/src/main/frontend/src/Components/Login.js
+++ MtdrSpring/backend/src/main/frontend/src/Components/Login.js
@@ -52,7 +52,7 @@ const Login = () => {
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                     />
-                    <button className = "button-login" type="submit">Prueba 3</button>
+                    <button className = "button-login" type="submit">Prueba 4</button>
                 </form>
             </div>
         </div>
